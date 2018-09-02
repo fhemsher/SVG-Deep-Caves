ffbff8f08515bdd68f9388ea4b48dd6dd540b81d
fhemsher <fhemsher@gmail.com>
2018-09-02 14:08:36 -0400

    Add files via upload

--- PUBLISH/showComment.js
+++ PUBLISH/showComment.js
@@ -35,24 +35,21 @@ function xml2txt(inputXml)
 	return "";
 
 }
-
-
 function showCaveComment(evt)
 {
-
-
-
 			var target=evt.target.parentNode
-
-  
-
 	  var comment=target.getAttribute("comment")
 	  if(comment)
 	  {
+         var lat=+target.getAttribute("lat")
+         var lng=+target.getAttribute("lng")
+         var gps="<br>GPS: "+lat.toFixed(6)+", "+lng.toFixed(6)
+         var email=target.getAttribute("createdBy")
+        var utcMS=+target.getAttribute("id").split("cave")[1].slice(0,13);  //
+       console.log(utcMS)
+        var time=new Date(utcMS).toUTCString()
 
-	     commentDiv.innerHTML=xml2txt(comment)
-
-
+	     commentDiv.innerHTML=xml2txt(comment)+gps+"<br>Contributed by: "+email+ "<br>" +time
 
 	     commentDiv.style.left=evt.clientX+10+"px"
 	     commentDiv.style.top=evt.clientY+30+"px"
@@ -60,8 +57,11 @@ function showCaveComment(evt)
 	     commentDiv.style.visibility="visible"
 
 	  }
+}
 
-
+function hideCaveComment(evt)
+{
+   		commentDiv.style.visibility="hidden"
 
 }
 
