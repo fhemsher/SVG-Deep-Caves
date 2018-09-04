75a2a4940b359b47c019e993fea280e62d7c0942
fhemsher <fhemsher@gmail.com>
2018-09-04 15:42:26 -0400

    Add files via upload

--- js00_utility/showComment.js
+++ js00_utility/showComment.js
@@ -2,6 +2,7 @@
 function showCaveComment(evt)
 {
 			var target=evt.target.parentNode
+
 	  var comment=target.getAttribute("comment")
 	  if(comment)
 	  {
@@ -10,10 +11,12 @@ function showCaveComment(evt)
          var gps="<br>GPS: "+lat.toFixed(6)+", "+lng.toFixed(6)
          var email=target.getAttribute("createdBy")
         var utcMS=+target.getAttribute("id").split("cave")[1].slice(0,13);  //
-       console.log(utcMS)
-        var time=new Date(utcMS).toUTCString()
 
-	     commentDiv.innerHTML=xml2txt(comment)+gps+"<br>Contributed by: "+email+ "<br>" +time
+        var time=new Date(utcMS).toUTCString()
+        var updated=""
+          if(target.getAttribute("update"))
+             updated="<br>Updated: "+target.getAttribute("update")
+	     commentDiv.innerHTML=xml2txt(comment)+gps+"<br>Contributed by: "+email+ "<br>" +time+updated
 
 	     commentDiv.style.left=evt.clientX+10+"px"
 	     commentDiv.style.top=evt.clientY+30+"px"
